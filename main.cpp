7688b001fb26d85770272082fbcc1de6677e22f9
Alice (MiramovTA) <alice@example.com>
2021-03-22 15:17:42 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Enter A and B: ";
+    int a, b;
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n';
     return 0;
 }
